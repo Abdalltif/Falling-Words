1713d540729676a1dd00036eb925f4a443abd8ff
Abdullateif <lp-ops-0035@LP-OPS-0035s-MacBook-Pro.local>
2022-05-03 06:34:20 +0200

    Activity and layout added

---
+++ app/src/main/java/com/abdullateif/fallingwords/ui/game/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.abdullateif.fallingwords.ui.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abdullateif.fallingwords.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
