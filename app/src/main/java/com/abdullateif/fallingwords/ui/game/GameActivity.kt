c0afe88df12bfdab5ae53474ab6ac7bc8021ae3a
Abdullateif <lp-ops-0035@LP-OPS-0035s-MacBook-Pro.local>
2022-05-03 06:48:06 +0200

    Animation implemented in activity

--- app/src/main/java/com/abdullateif/fallingwords/ui/game/GameActivity.kt
+++ app/src/main/java/com/abdullateif/fallingwords/ui/game/GameActivity.kt
@@ -2,11 +2,42 @@ package com.abdullateif.fallingwords.ui.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.abdullateif.fallingwords.R
+import com.abdullateif.fallingwords.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun startFallingAnimation() {
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fall_down)
+        animation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                cancelFallingAnimation()
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+            }
+        })
+
+        binding.tvTranslation.startAnimation(animation)
+    }
+
+    private fun cancelFallingAnimation() {
+        binding.tvTranslation.animation?.setAnimationListener(null)
+        binding.tvTranslation.clearAnimation()
     }
 }
