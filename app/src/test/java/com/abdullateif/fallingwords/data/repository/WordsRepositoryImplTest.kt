5808e45e9b7ebf8aca29e8182c2eb2164e9e2107
Abdullateif <lp-ops-0035@LP-OPS-0035s-MacBook-Pro.local>
2022-05-03 07:58:44 +0200

    repository unit test added

--- app/src/test/java/com/abdullateif/fallingwords/data/repository/WordsRepositoryImplTest.kt
+++ app/src/test/java/com/abdullateif/fallingwords/data/repository/WordsRepositoryImplTest.kt
@@ -1,5 +1,59 @@
 package com.abdullateif.fallingwords.data.repository
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.abdullateif.fallingwords.common.Resource
+import com.abdullateif.fallingwords.data.source.WordsDataSource
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
 
-class WordsRepositoryImplTest
+
+@ExperimentalCoroutinesApi
+class WordsRepositoryImplTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private val dataSource = mockk<WordsDataSource>()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @Test
+    fun `fetch data successfully by mock data`() = runTest {
+        val mockWordList = MockWordsProvider.getMockWords()
+
+        coEvery { dataSource.fetchWords() } returns Resource.Success(data = mockWordList)
+
+        val result = dataSource.fetchWords()
+
+        coVerify { dataSource.fetchWords() }
+
+        assertThat(result).isInstanceOf(Resource.Success::class.java)
+        assertThat(result.data).isNotNull()
+    }
+
+    @Test
+    fun `fetch data with exception`() = runTest {
+        coEvery { dataSource.fetchWords() } returns Resource.Error(message = "error")
+
+        val result = dataSource.fetchWords()
+
+        coVerify { dataSource.fetchWords() }
+
+        assertThat(result).isInstanceOf(Resource.Error::class.java)
+        assertThat(result.data).isNull()
+    }
+}
